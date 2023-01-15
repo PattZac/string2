4899e5bb16c56b1d78ba8136a8bf57744a383f06
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-15 13:50:20 +0100

    Add files via upload

---
+++ string2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            s = s.Replace(' ', '_');
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
